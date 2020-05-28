56b249ac17d18bf06750f62c817ace3a13fcbb36
Debdatta123 <51695675+Debdatta123@users.noreply.github.com>
2020-05-29 00:40:30 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Topsection from "./Topsection";
+export default function App() {
+  return (
+    <div>
+      <Topsection />
+    </div>
+  );
+}
+
---
+++ src/components/Topsection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Topsection() {
+  return (
+    <div class="curved">
+      <h1>Welcome to weather app</h1>
+      <p>Predict weather</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#fff"
+          fill-opacity="1"
+          d="M0,160L60,160C120,160,240,160,360,181.3C480,
+      203,600,245,720,250.7C840,256,960,224,1080,213.3C1200,203,1320,213,1380,
+      218.7L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,
+      320,720,320C600,320,480,320,360,320C240,320,120,320,60,
+      320L0,320Z"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default Topsection;
